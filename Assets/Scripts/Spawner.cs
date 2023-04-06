327bf30c0b31b64c340405e55cd3c9509c7fb6e1
mirindra rabenjarijaona <hery.rabenjarijaona@epita.fr>
2023-04-06 16:39:58 +0200

    fini

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -11,7 +11,7 @@ public class Spawner : MonoBehaviour
     {
         listnpc = GameObject.FindGameObjectsWithTag("NPC");
 
-        if (listnpc.Length < 1)
+        if (listnpc.Length < 10)
         {
             Vector3 randomSpawn = new Vector3(Random.Range(-13,11), 6 , Random.Range(-18,0));
             Instantiate(npc,randomSpawn,Quaternion.identity);
