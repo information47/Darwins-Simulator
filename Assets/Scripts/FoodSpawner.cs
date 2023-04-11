0b9b7283954ebe8b5ac43bd10214f7d4c223277e
mirindra rabenjarijaona <hery.rabenjarijaona@epita.fr>
2023-04-11 10:22:47 +0200

    nourriture bien

--- Assets/Scripts/FoodSpawner.cs
+++ Assets/Scripts/FoodSpawner.cs
@@ -7,25 +7,33 @@ public class FoodSpawner : MonoBehaviour
 {
     public GameObject food;
     public GameObject[] listFood;
+    public int timer = 0;
 
     // Update is called once per frame
     void Update()
     {
         listFood = GameObject.FindGameObjectsWithTag("Food");
-        Spawn();
+        timer++;
         
+        if (timer > 5000)
+        {
+            timer = 0;
+            Spawn();
+        }
+
     }
 
     private void Spawn()
     {
-        if (listFood.Length == 0)
+        if (listFood.Length < 10 - listFood.Length )
         {
-            for (int i = 0; i < 11; i++)
+            for (int i = 0; i < 10 - listFood.Length; i++)
             {
                 Vector3 randomSpawn = new Vector3(Random.Range(-13, 11), 6, Random.Range(-18, 0));
                 Instantiate(food, randomSpawn, Quaternion.identity);
             }
         }
-        
     }
+
+
 }
