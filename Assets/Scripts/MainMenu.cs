0dab419fe9af2ad29e60dfca9cfa0c5c7a324bd5
Ruben <85025254+Kaylua@users.noreply.github.com>
2023-04-11 02:12:50 +0200

    Menu Boutons Fonctionnels

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,17 +1,55 @@
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using System.Linq;
 
 public class MainMenu : MonoBehaviour
 {
     public void StartGame(string sceneName)
     {
-        Debug.Log("Le joueur a quitté le jeu.");
-        SceneManager.LoadScene(sceneName);
+        Debug.Log("Start Game");
+        //SceneManager.LoadScene(sceneName);
+
     }
 
     public void QuitGame()
     {
         Application.Quit();
-        Debug.Log("Le joueur a quitté le jeu.");
+        Debug.Log("Quit Game");
     }
+
+    public void Settings()
+    {
+        Debug.Log("Parametres");
+    }
+
+    public void Sound()
+    {
+        Debug.Log("Sound");
+    }
+
+    public void FocusMainCamera()
+    {
+        // Trouver la caméra principale dans la scène
+        Camera mainCamera = Camera.main;
+
+        // Vérifier si la caméra a été trouvée
+        if (mainCamera != null)
+        {
+            // Changer la position de la caméra actuelle à celle de la caméra principale
+            if (Camera.current != null)
+            {
+                Camera.current.transform.position = mainCamera.transform.position;
+                Camera.current.transform.rotation = mainCamera.transform.rotation;
+            }
+            else
+            {
+                Debug.LogError("La caméra actuelle n'a pas été trouvée.");
+            }
+        }
+        else
+        {
+            Debug.LogError("La caméra principale n'a pas été trouvée dans la scène.");
+        }
+    }
+
 }
