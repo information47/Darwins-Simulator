a82ac18b987e2dff9bb7c4f55edb16deaaacd44b
rpalmier <rpalmier@et.intechinfo.fr>
2023-04-11 22:13:16 +0200

    camera marche mais position aleatoire

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,13 +5,14 @@ public class Spawner : MonoBehaviour
 {
     public GameObject npc;
     public GameObject[] listnpc;
+    public int test = 2;
 
     // Update is called once per frame
     void Update()
     {
         listnpc = GameObject.FindGameObjectsWithTag("NPC");
 
-        if (listnpc.Length < 10)
+        if (listnpc.Length < 4)
         {
             Vector3 randomSpawn = new Vector3(Random.Range(-13,11), 6 , Random.Range(-18,0));
             Instantiate(npc,randomSpawn,Quaternion.identity);
--- Assets/Scripts/SwitchView.cs
+++ Assets/Scripts/SwitchView.cs
@@ -5,28 +5,24 @@ using Cinemachine;
 
 public class SwitchView : MonoBehaviour
 {
-    [SerializeField] private CinemachineVirtualCamera cineCam;
-    [SerializeField] private List<Transform> targets; //Liste des Transform que l'on veut suivre
+    public Camera thirdPersonCamera;
+    public GameObject[] targets;
+    private int currentTargetIndex;
 
-    public int currentInt;
-    // Start is called before the first frame update
     void Start()
     {
-        cineCam = GetComponent<CinemachineVirtualCamera>();
+        targets = GameObject.FindGameObjectsWithTag("NPC");
+        currentTargetIndex = 0;
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
-        { // appui sur la touche espace
-            Debug.Log("switch target");
-            currentInt++;
-            if (currentInt >= targets.Count)
-            {
-                currentInt = 0;
-            }
-            cineCam.Follow = targets[currentInt]; // change le paramètre Follow pour un nouveau Transform pris dans la liste
+        {   
+            currentTargetIndex = (currentTargetIndex + 1) % targets.Length;
+            Transform target = targets[currentTargetIndex].transform;
+            thirdPersonCamera.transform.position = target.Find("Camera Pivot").position;
+            thirdPersonCamera.transform.rotation = target.Find("Camera Pivot").rotation;
         }
     }
 }
