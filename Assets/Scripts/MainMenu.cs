8fd4b3e3d5aaf68cbd967fe435b672e72540dcb9
Ruben <85025254+Kaylua@users.noreply.github.com>
2023-04-12 10:39:41 +0200

    Menu fonctionnel

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,60 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup myUIGroup;
+    public void ShowMenu()
+    {
+        Debug.Log("ShowMenu");
+        // Rends le Canva visible en mettant sa valeur alpha à 1
+        myUIGroup.alpha = 1;
+        // Stop la mise à jour des objets du jeu en mettant timeScale à 0
+        Time.timeScale = 0f;
+    }
+
+    public void HideMenu()
+    {
+        Debug.Log("HideMenu");
+        // Rends le Canva non visible en mettant sa valeur alpha à 0
+        myUIGroup.alpha = 0;
+        // Relance la mise à jour des objets du jeu en mettant timeScale à 1
+        Time.timeScale = 1f;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit Game");
+    }
+
+    public void Settings()
+    {
+        Debug.Log("Parametres");
+    }
+
+    public void Sound()
+    {
+        Debug.Log("Sound");
+    }
+
+    private void Start()
+    {
+        // Stop le jeu tant que le joueur n'appuis pas sur PLAY
+        Time.timeScale = 0f;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ShowMenu();
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            HideMenu();
+        }
+    }
+}
