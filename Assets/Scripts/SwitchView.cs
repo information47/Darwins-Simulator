bff5d37a7a82bd28e886bd868cbc54dbc216d1da
Robin <rpalmier@et.intechinfo.fr>
2023-04-17 09:44:17 +0200

    changement de branche

--- Assets/Scripts/SwitchView.cs
+++ Assets/Scripts/SwitchView.cs
@@ -18,11 +18,11 @@ public class SwitchView : MonoBehaviour
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
-        {   
-            currentTargetIndex = (currentTargetIndex + 1) % targets.Length;
-            Transform target = targets[currentTargetIndex].transform;
-            thirdPersonCamera.transform.position = target.Find("Camera Pivot").position;
-            thirdPersonCamera.transform.rotation = target.Find("Camera Pivot").rotation;
+        {
+         currentTargetIndex = (currentTargetIndex + 1) % targets.Length;
+         Transform target = targets[currentTargetIndex].transform;
+         thirdPersonCamera.transform.position = target.Find("Camera Pivot").position;
+         thirdPersonCamera.transform.rotation = target.Find("Camera Pivot").rotation;
         }
     }
 }
