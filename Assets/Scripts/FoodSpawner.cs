a6965dd1c657c38ef13aa04ac1e35f2b54d003c7
PALMIER Robin <rpalmier@et.intechinfo.fr>
2023-04-17 09:36:29 +0000

    feature/Animation2

---
+++ Assets/Scripts/FoodSpawner.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+public class FoodSpawner : MonoBehaviour
+{
+    public GameObject food;
+    public GameObject[] listFood;
+    public int timer = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        listFood = GameObject.FindGameObjectsWithTag("Food");
+        timer++;
+        
+        if (timer > 2500)
+        {
+            timer = 0;
+            Spawn();
+        }
+
+    }
+
+    private void Spawn()
+    {
+        if (listFood.Length < 10 - listFood.Length )
+        {
+            for (int i = 0; i < 15 - listFood.Length; i++)
+            {
+                Vector3 randomSpawn = new Vector3(Random.Range(-11, 10), 6, Random.Range(-36, -54));
+                Instantiate(food, randomSpawn, Quaternion.identity);
+            }
+        }
+    }
+
+
+}
